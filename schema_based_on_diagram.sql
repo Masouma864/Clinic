fb06d6f59e79fade7d9848615d26bdd39d85f4c2
masouma <masouma2020@gmail.com>
2022-12-20 19:19:41 +0430

    Create tables according to diagram

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,47 @@
+create table patients(id serial primary key , name varchar(50) , date_of_birth date);
+
+create table invoices(
+    id serial primary key , 
+    total_amount decimal , 
+    generated_at timestamp ,
+    payed_at timestamp,
+    medical_history_id int,
+    constraint fk 
+    foreign key medical_history_id 
+    references medical_histories(id) 
+    on delete cascade
+    );
+
+    create table medical_histories(
+        id serial primary key ,
+        admitted_at timestamp,
+        patient_id int,
+        status varchar default active,
+        constraint fk 
+       foreign key patient_id
+       references patients(id)
+    );
+
+    create table invoice_items(
+        id serial primary key,
+        unit_price decimal,
+        quantity int,
+        total_price decimal,
+        invoice_id int,
+        treatment_id int
+        constraint fk foreign key invoice_id references invoices(id)on delete cascade,
+        constraint fk foreign key treatment_id references treatments(id)on delete cascade
+        );
+
+    create table treatments(
+        id serial primary key,
+        type varchar(10),
+        name varchar(20),
+        );    
+
+    CREATE TABLE medical_histories_has_treatments (
+       medical_history_id int refrences medical_histories(id),
+       treatment_id int refrences treatments(id),
+    );    
+
+   
