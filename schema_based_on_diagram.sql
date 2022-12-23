a21289f2521e4d70e22ab9fccf7974d807bf855f
masouma <masouma2020@gmail.com>
2022-12-23 18:04:53 +0430

    update schema

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,49 +1,40 @@
-create table patients(id serial primary key , name varchar(50) , date_of_birth date);
+CREATE TABLE patients (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(200) NOT NULL,
+  date_of_birth DATE NOT NULL
+);
 
-create table invoices(
-    id serial primary key , 
-    total_amount decimal , 
-    generated_at timestamp ,
-    payed_at timestamp,
-    medical_history_id int,
-    constraint fk 
-    foreign key medical_history_id 
-    references medical_histories(id) 
-    on delete cascade
-    );
+CREATE TABLE medical_histories (
+  id BIGSERIAL PRIMARY KEY,
+  admitted_at TIMESTAMP NOT NULL,
+  patient_id INT REFERENCES patients(id) NOT NULL,
+  status VARCHAR(200) NOT NULL
+);
 
-    create table medical_histories(
-        id serial primary key ,
-        admitted_at timestamp,
-        patient_id int,
-        status varchar default active,
-        constraint fk 
-       foreign key patient_id
-       references patients(id)
-    );
+CREATE TABLE invoices (
+  id BIGSERIAL PRIMARY KEY,
+  total_amount DECIMAL NOT NULL,
+  generated_at TIMESTAMP NOT NULL,
+  payed_at TIMESTAMP,
+  medical_history_id INT
+);
 
-    create table invoice_items(
-        id serial primary key,
-        unit_price decimal,
-        quantity int,
-        total_price decimal,
-        invoice_id int,
-        treatment_id int
-        constraint fk foreign key invoice_id references invoices(id)on delete cascade,
-        constraint fk foreign key treatment_id references treatments(id)on delete cascade
-        );
+CREATE TABLE treatments (
+  id BIGSERIAL PRIMARY KEY,
+  type VARCHAR(200) NOT NULL,
+  name VARCHAR(200) NOT NULL
+);
 
-    create table treatments(
-        id serial primary key,
-        type varchar(10),
-        name varchar(20),
-        );    
+CREATE TABLE invoice_items (
+  id BIGSERIAL PRIMARY KEY,
+  unit_price DECIMAL NOT NULL,
+  quantity INT NOT NULL,
+  total_price DECIMAL NOT NULL,
+  invoice_id INT REFERENCES invoices(id) NOT NULL,
+  treatement_id INT REFERENCES treatments(id)
+);
 
-    CREATE TABLE medical_histories_has_treatments (
-       medical_history_id int refrences medical_histories(id),
-       treatment_id int refrences treatments(id),
-    );    
-
-   create index patient_id_medical_histories on medical_histories(patient_id);
-    create index invoices_medical_histories on invoices(medical_history_id);
-    create index invoice_items_treatment_id on invoice_items(treatment_id);
+CREATE TABLE medical_histories_has_treatments (
+  medical_history_id INT REFERENCES medical_histories(id),
+  treatment_id INT REFERENCES treatments(id)
+);
